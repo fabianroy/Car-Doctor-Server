d73e88f0baa8b687e8041d734adfee189bda7f72
Fabian Roy <fabianroylikhon@gmail.com>
2024-05-08 01:16:07 +0600

    Added JWT

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 const env = require('dotenv').config();
 
 // middle wire
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
+
 app.use(express.json());
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -34,6 +41,21 @@ async function run() {
         const bookCollection = client.db("CarDoctor").collection("bookings");
         const userCollection = client.db("CarDoctor").collection("users");
 
+        // auth related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false, // http://localhost:5173/login
+
+                })
+                .send({success: true});
+        })
+
+        // Services
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
@@ -55,6 +77,7 @@ async function run() {
         // Bookings
 
         app.get('/bookings', async (req, res) => {
+            console.log('token', req.cookies.token);
             let query = {}
             if (req.query.email) {
                 query = { email: req.query.email }
